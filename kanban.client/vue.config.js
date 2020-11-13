9d07be55343cb04b5b51349893832866990a1fa8
Dan <cunningdan@hotmail.com>
2020-11-13 10:40:38 -0700

    commit

--- kanban.client/vue.config.js
+++ kanban.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '../kanban.server/client',
-  outputDir: 'docs',
+// NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  publicPath: '/',
+  outputDir: '../kanban.server/client',
   productionSourceMap: false
 }
